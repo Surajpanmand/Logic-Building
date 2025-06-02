61e25c0d5efc46acea997737ad04a10920d9330d
Code-4ge <panmandneeraj@gmail.com>
2025-06-03 00:12:08 +0530

    Logic-Buiging Program 10-30

---
+++ program27.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+#define ERR_INVALID -1
+
+int Factroial(int iNo)
+
+{
+    int iCnt = 0;
+    int ifact = 1;
+
+    if(iNo < 0)
+    {
+        return ERR_INVALID;
+    }
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        ifact = ifact * iCnt;
+    }
+    return ifact;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number");
+    scanf("%d",&iValue);
+
+    iRet = Factroial(iValue);
+
+    if(iRet == ERR_INVALID)
+    {
+        printf("invalid");
+    }
+    else
+    {
+    printf("Factroial is %d",iRet);
+    }
+
+    return 0;
+}
